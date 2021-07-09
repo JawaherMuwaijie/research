519f415fb326c9f93c579d141a82b79bd4eeaae3
Uljad Berdica <ub352@nyu.edu>
2021-07-10 00:37:16 +0400

    Updates

---
+++ precache-manifest.1b7fe8471defd3e637c7ee797e132bc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f6e565664a546ff1b5f39aa645b473",
+    "url": "/uljadberdica/index.html"
+  },
+  {
+    "revision": "588bc2408dc36f27c42a",
+    "url": "/uljadberdica/static/css/2.9e3fc118.chunk.css"
+  },
+  {
+    "revision": "fb3dad31d877cda822ca",
+    "url": "/uljadberdica/static/css/main.97252073.chunk.css"
+  },
+  {
+    "revision": "588bc2408dc36f27c42a",
+    "url": "/uljadberdica/static/js/2.e5ef10e6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/uljadberdica/static/js/2.e5ef10e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb3dad31d877cda822ca",
+    "url": "/uljadberdica/static/js/main.04e3c0f3.chunk.js"
+  },
+  {
+    "revision": "ee6ea9ab41d9bcd68435",
+    "url": "/uljadberdica/static/js/runtime-main.299b2b82.js"
+  }
+]);
