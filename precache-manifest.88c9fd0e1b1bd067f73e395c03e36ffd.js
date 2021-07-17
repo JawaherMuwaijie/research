a3b1c5b386a3a97e3e3d36bb134a9e9bb843df06
Uljad Berdica <ub352@nyu.edu>
2021-07-18 00:11:56 +0400

    Updates

---
+++ precache-manifest.88c9fd0e1b1bd067f73e395c03e36ffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34688389adf5e2d4339a7a7e85651bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "588bc2408dc36f27c42a",
+    "url": "/static/css/2.9e3fc118.chunk.css"
+  },
+  {
+    "revision": "f98e08419f5c146bddaa",
+    "url": "/static/css/main.dac4b123.chunk.css"
+  },
+  {
+    "revision": "588bc2408dc36f27c42a",
+    "url": "/static/js/2.e5ef10e6.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.e5ef10e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f98e08419f5c146bddaa",
+    "url": "/static/js/main.f627d806.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/research/precache-manifest.7644c3f43a1e4a39c5bd760b636705ca.js"
+  "/precache-manifest.88c9fd0e1b1bd067f73e395c03e36ffd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/research/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
